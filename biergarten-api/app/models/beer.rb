468381469296b8e4b2b4884ad7fcbacdbba1682f
Kevin Neyer <kevinneyer@gmail.com>
2025-12-04 12:13:36 -0500

    cleanup old code

--- biergarten-api/app/models/beer.rb
+++ biergarten-api/app/models/beer.rb
@@ -3,5 +3,4 @@ class Beer < ApplicationRecord
   has_many :users, through: :reviews
   has_many :favorites
   has_many :users, through: :favorites
-  has_and_belongs_to_many :likes
 end
--- biergarten-api/app/models/user.rb
+++ biergarten-api/app/models/user.rb
@@ -9,8 +9,6 @@ class User < ApplicationRecord
   has_many :passive_relationships, class_name: "Relationship", foreign_key: :followed_id, dependent: :destroy
   has_many :followers, through: :passive_relationships, source: :follower
 
-  has_and_belongs_to_many :likes
-
   validates :username, uniqueness: true
 
   has_secure_password
