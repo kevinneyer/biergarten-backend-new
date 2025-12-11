03141b0f331ae83fb5bad5243ba82754fb41105d
Kevin Neyer <kevinneyer@gmail.com>
2025-12-11 09:39:37 -0500

    update like controller and model for post request

--- biergarten-api/app/controllers/api/v1/beers_controller.rb
+++ biergarten-api/app/controllers/api/v1/beers_controller.rb
@@ -7,7 +7,7 @@ class Api::V1::BeersController < ApplicationController
 
     def show
         beer = Beer.find(params[:id])
-        render json: beer 
+        render json: beer
     end
 
     def update
@@ -19,8 +19,8 @@ class Api::V1::BeersController < ApplicationController
     def like
       beer = Beer.find(params[:id])
 
-      if (session_user) 
-        session_user.likes.create(beerId: beer.id)
+      if (session_user)
+        session_user.likes.create(beer_id: beer.id)
       else
         render json: { body: "ERROR"}
       end
