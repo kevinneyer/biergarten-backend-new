40e530e2f7ae8a04a83e477a455f55de6540e1a5
Kevin Neyer <kevinneyer@gmail.com>
2025-12-23 19:28:33 -0500

    update beers controller show function to have nested json

--- biergarten-api/app/controllers/api/v1/beers_controller.rb
+++ biergarten-api/app/controllers/api/v1/beers_controller.rb
@@ -7,7 +7,17 @@ class Api::V1::BeersController < ApplicationController
 
     def show
         beer = Beer.find(params[:id])
-        render json: beer
+        is_liked = false
+
+        if session_user
+            like_array = session_user.likes.select{ |like| like.beer_id == beer.id}
+            is_liked = like_array.length > 0
+        end
+
+        render json: {
+            beer: ActiveModelSerializers::SerializableResource.new(beer, each_serializer: BeerSerializer), 
+            is_liked: is_liked
+        }
     end
 
     def update
